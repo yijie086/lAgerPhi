8bcc46f9c4985ad544a8b8c0e137ae6d24460558
Sylvester Joosten <sylvester.joosten@gmail.com>
2016-04-29 05:10:12 -0400

    bugfix

--- pcsim/physics/pc.hh
+++ pcsim/physics/pc.hh
@@ -18,14 +18,16 @@ namespace physics {
 class pc_xsec : public spline {
 public:
   pc_xsec(const ptree& settings, const string_path& path)
-      : spline(settings, path), coupling{conf().get<double>("coupling")} {}
+      : spline(settings, path), coupling_{conf().get<double>("coupling")} {}
 
   // This function returns the cross-section for a charmed pentaquark based on a
   // spline fit to the cross section (to be given through a configuration file)
   // as a function of W only, based on Qian Wang's paper: Phys.Rev. D92 (2015)
   // 034022.  It ignores any angular considerations for the cross-section.
   // Returned result is in nb.
-  double operator(const double W) const { return coupling_ * spline::eval(W); }
+  double operator()(const double W) const {
+    return coupling_ * spline::eval(W);
+  }
 
 private:
   const double coupling_;
