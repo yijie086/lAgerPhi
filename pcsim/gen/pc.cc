9a604b14471c295c1733042bf9f58ba1cdc1f792
Sylvester Joosten <sylvester.joosten@gmail.com>
2016-04-29 05:11:01 -0400

    implemented Pc generator

---
+++ pcsim/gen/pc.cc
@@ -0,0 +1,58 @@
+#include "pc.hh"
+#include <pcsim/physics/decay.hh>
+#include <pcsim/physics/pdg.hh>
+
+namespace pcsim {
+namespace gen {
+
+pc::pc(const ptree& settings, const string_path& path,
+       std::shared_ptr<TRandom> r)
+    : base_type{settings, path, "s-channel Charmed Pentaquark Generator",
+                std::move(r)}
+    , xsec_{settings, path / "xsec"}
+    , me_{physics::PDG_ELECTRON.Mass()}
+    , Mjp_{physics::PDG_JPSI.Mass()}
+    , Mp_{physics::PDG_PROTON.Mass()}
+    , Wjp_{physics::PDG_JPSI_WIDTH}
+    , ctheta_min_{-1.}
+    , ctheta_max_{1.} {}
+
+jpsi_event pc::gen_impl(const photon_beam& photon) {
+  jpsi_event ev;
+
+  // setup the initial state
+  ev.beam.SetPxPyPzE(0, 0, photon.energy, photon.energy);
+  ev.target.SetPxPyPzE(0, 0, 0, Mp_);
+
+  // reaction in the photon-proton CM frame
+  const auto cm = (ev.beam + ev.target);
+  const auto beta_cm = cm.BoostVector();
+  ev.s = cm.M2();
+  ev.W = std::sqrt(ev.s);
+
+  // get the cross section
+  ev.xsec = xsec_(ev.W);
+  ev.weight = ev.xsec;
+  // bail if the cross section is zero
+  if (!(ev.xsec > 0)) {
+    ev.good = false;
+    return ev;
+  }
+
+  // create our Pc and boost to the lab frame
+  TLorentzVector pc{0, 0, 0, ev.W};
+  pc.Boost(beta_cm);
+
+  // get the decay proton and J/Psi
+  physics::decay_pc_iso(rng(), pc, ev.recoil, ev.jpsi);
+
+  // get the J/Psi decay leptons
+  physics::decay_jpsi_lepton(rng(), ev.jpsi, me_, ev.positron, ev.electron);
+
+  // all done!
+  ev.good = true;
+  return ev;
+}
+
+} // gen
+} // pcsim
---
+++ pcsim/gen/pc.hh
@@ -0,0 +1,37 @@
+#ifndef PCSIM_GEN_PC_LOADED
+#define PCSIM_GEN_PC_LOADED
+
+#include <TLorentzVector.h>
+#include <TRandom.h>
+#include <pcsim/core/configuration.hh>
+#include <pcsim/core/generator.hh>
+#include <pcsim/gen/bremsstrahlung.hh>
+#include <pcsim/gen/jpsi.hh>
+#include <pcsim/physics/pc.hh>
+
+namespace pcsim {
+namespace gen {
+
+class pc : public generator<pc, jpsi_event> {
+public:
+  using base_type = generator<pc, jpsi_event>;
+
+  pc(const ptree& settings, const string_path& path,
+       std::shared_ptr<TRandom> r);
+
+  jpsi_event gen_impl(const photon_beam& photon);
+
+private:
+  const physics::pc_xsec xsec_;
+  const double me_;  // electron mass
+  const double Mjp_; // J/Psi pole mass
+  const double Mp_;  // proton mass
+  const double Wjp_; // J/Psi decay width
+  const double ctheta_min_; // min and max cos(theta) that can be reached (equal
+  const double ctheta_max_; // to -1 and +1)
+};
+
+} // gen
+} // pcsim
+
+#endif
