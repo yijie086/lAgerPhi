6df10023002038fa294bafd9f7a602a1cdb7f1a0
Sylvester Joosten <sylvester.joosten@gmail.com>
2016-05-02 16:29:57 -0400

    made Pc cross section configurable

--- pcsim/physics/pc.hh
+++ pcsim/physics/pc.hh
@@ -1,42 +1,38 @@
 #ifndef PCSIM_PHYSICS_PC_DEFINED
 #define PCSIM_PHYSICS_PC_DEFINED
 
-#include <pcsim/core/configuration.hh>
-
 #include <TMath.h>
-
 #include <cmath>
-
+#include <pcsim/core/configuration.hh>
 
 // cross section definitions
 namespace pcsim {
 namespace physics {
 
-// Cross section 
+// Cross section: using a crude gaussian fit to the the results from
+//    Wang, Qian, Xiao-Hai Liu, and Qiang Zhao. 2015. “Photoproduction of Hidden
+//    Charm Pentaquark States Pc+(4380)and Pc+(4450).” Physical Review D 92 (3):
+//    034022–27. doi:10.1103/PhysRevD.92.034022.
+// numbers, needs MAJOR refinements!
 // Values are in units of nb/GeV^2
-class pc_xsec : public generator {
+class pc_xsec : public configurable {
 public:
   pc_xsec(const ptree& settings, const string_path& path)
-      : configurable(settings, path), coupling_{conf().get<double>("coupling")} {}
+      : configurable(settings, path)
+      , max_{conf().get<double>("max")}
+      , mean_{conf().get<double>("mean")}
+      , sigma_{conf().get<double>("width") / 2.}
+      , coupling_{conf().get<double>("coupling")} {}
 
-#if 0
-  // This function returns the cross-section for a charmed pentaquark based on a
-  // spline fit to the cross section (to be given through a configuration file)
-  // as a function of W only, based on Qian Wang's paper: Phys.Rev. D92 (2015)
-  // 034022.  It ignores any angular considerations for the cross-section.
-  // Returned result is in nb.
-  double operator()(const double W) const {
-    return coupling_ * spline::eval(W);
-  }
-#endif
-  // crude gaussian fit to the interpolated numbers, needs MAJOR
-  // refinements!
   double operator() (const double W) const {
-    return coupling_ * 10000. * TMath::Gaus(W, 4.450, .02);
+    return coupling_ * max_ * TMath::Gaus(W, mean_, sigma_);
   }
 
 private:
-  const double coupling_;
+  const double max_;      // maximum cross section value
+  const double mean_;     // cross section mean
+  const double sigma_;    // cross section width
+  const double coupling_; // branching ratio of Pc -> J/Psi+p channel
 };
 
 } // physics
