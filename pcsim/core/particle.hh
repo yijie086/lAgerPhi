889d3169f5d48407dfa7ffa79f0c39b961e283eb
Sylvester Joosten <sylvester.joosten@gmail.com>
2017-03-23 17:40:10 -0400

    migrated from using the TVector3 etc classes to Math::XYZVector etc

--- pcsim/core/particle.hh
+++ pcsim/core/particle.hh
@@ -1,9 +1,11 @@
 #ifndef PCSIM_CORE_PARTICLE_LOADED
 #define PCSIM_CORE_PARTICLE_LOADED
 
-#include <TLorentzVector.h>
+#include <Math/LorentzRotation.h>
+#include <Math/Vector3D.h>
+#include <Math/Vector4D.h>
+
 #include <TRandom.h>
-#include <TVector3.h>
 #include <memory>
 #include <pcsim/core/interval.hh>
 #include <pcsim/core/pdg.hh>
@@ -13,6 +15,11 @@ namespace pcsim {
 // particle info
 class particle {
 public:
+  using ROOT::Math::XYZTVector::BetaVector;
+  using ROOT::Math::XYZTVector;
+  using ROOT::Math::Boost;
+  using ROOT::Math::XYZVector;
+
   // particle status
   enum class status_code : int {
     BEAM = 11,
@@ -29,34 +36,17 @@ public:
   particle() = default;
   particle(const particle& rhs) = default;
   // particle with a given id
-  particle(const pdg_id id)
-      : type_{id}
-      , pdg_{pdg_particle(id)}
-      , charge_{static_cast<int>(pdg->Charge() / 3)} {}
+  particle(const pdg_id id);
   // particle with a given momentum 3-vector
-  particle(const pdg_id id, const TVector3& p3)
-      : particle{id}
-      , mass_{pdg->Mass()}
-      , p_{p3, sqrt(mass_ * mass_ + p_.Mag2())} {}
+  particle(const pdg_id id, const XYZVector& p3);
   // particle in a given direction with a given energy
-  particle(const pdg_id id, const TVector3 direction, const double E)
-      : particle{id}
-      , mass_{pdg->Mass()}
-      , p_{direction.Unit() * sqrt(E * E - mass_ * mass_), E} {}
+  particle(const pdg_id id, const XYZVector& direction, const double E);
   // particle with a given 4-momentum (can be off-shell)
-  particle(const pdg_id id, const TLorentzVector p) {
-    particle{id}, mass_{p.M()}, p_{p} {}
-  }
+  particle(const pdg_id id, const XYZTVector& p);
   // RNG constructors usefull in particular for unstable particles with a mass
   // and lifetime that needs to be generated
-  particle(const pdg_id id, std::shared_ptr<TRandom> rng) : particle(id) {
-    set_mass_lifetime(std::move(rng));
-  }
-  particle(const pdg_id id, const TVector3& p3, std::shared_ptr<TRandom> rng)
-      : particle{id} {
-    set_mass_lifetime(std::move(rng));
-    p_.SetXYZM(p.X(), p.Y(), p.Z(), mass_);
-  }
+  particle(const pdg_id id, std::shared_ptr<TRandom> rng);
+  particle(const pdg_id id, const XYZVector& p3, std::shared_ptr<TRandom> rng);
 
   // particle ID code
   pdg_id type() const { return type_; }
@@ -84,44 +74,28 @@ public:
   int n_parents() const { return parent_.max - parent_.min; }
 
   // get a reference to the momentum/vertex 4-vector
-  TLorentzVector& p() { return p_; };
-  const TLorentzVector& p() const { return p_; };
-  TLorentzVector& vertex() { return vertex_; }
-  const TLorentzVector& vertex() const { return vertex_; }
+  XYZTVector& p() { return p_; };
+  const XYZTVector& p() const { return p_; };
+  XYZTVector& vertex() { return vertex_; }
+  const XYZTVector& vertex() const { return vertex_; }
+
+  // transformations etc.
+  void boost(const BetaVector& bv);
+  void boost(const Boost& b) { p = b * p; }
+  // rotate from a coordate system where v moved along the z-axis, to the
+  // coordate system of v
+  // (algorithm taken from TVector3::RotateUz)
+  void rotate_uz(const XYZVector& v);
 
   // add a daughter track
-  void add_daughter(const int index) {
-    if (daughter_.min > index || daughter_.min < 0) {
-      daughter_.min = index;
-    }
-    if (daughter_.max >= index || daughter_.max < 0) {
-      daughter_.max = index + 1;
-    }
-  }
-  void add_parent(const int index) {
-    if (parent_.min < 0) {
-      parent_.min = index;
-    } else if (parent_.max < 0) {
-      parent_.max = index;
-    } else {
-      tassert(false, "A particle can have only have up to 2 parents"
-                     "(tried to add additional parent to particle '" +
-                         std::string(pdg->Name()) + "')");
-    }
-  }
+  void add_daughter(const int index);
+  // add a parent track
+  void add_parent(const int index);
+  // set the parent tracks
   void set_parents(const interval<int> indices) { parent_ = indices; }
 
 private:
-
-  void set_mass_lifetime(std::shared_ptr<TRandom> rng) {
-    if (pdg->Stable()) {
-      mass = pdg->Mass();
-      lifetime = 0;
-    else {
-      mass = rng->BreitWigner(pdg->Mass(), pdg->Width());
-      lifetime = rng->Exp(pdg->Lifetime());
-    }
-  }
+  void set_mass_lifetime(std::shared_ptr<TRandom> rng);
 
   pdg_id type_{pdg_id::UNKNOWN};
   TParticlePDG* pdg_{nullptr};
@@ -132,15 +106,124 @@ private:
   // for unstable particles!
   double mass_{0};
   double lifetime_{0};
-  TLorentzVector p_{0, 0, 0, 0};
-  TLorentzVector vertex_{0, 0, 0, 0};
-  // parent indices store the first (and optional second) parent of a particle.
-  // -1 if not stored
+  XYZTVector p_{0, 0, 0, 0};
+  XYZTVector vertex_{0, 0, 0, 0};
+  // parent indices store the first (and optional second) parent of a
+  // particle. -1 if not stored
   interval<int> parent_{-1, -1};
   // daughter indices are encoded from [begin, end) where begin is the
   // first index and end one past the last index. (-1, -1) if none
   interval<int> daughter_{-1, -1};
 };
+
+} // namespace pcsim
+
+// =============================================================================
+// Particle implementation
+//
+// This is a work-horse class called, and therefore all its members are defined
+// inline
+// =============================================================================
+namespace pcsim {
+
+//
+// CONSTRUCTORS
+//
+// particle with a given id
+inline particle::particle(const pdg_id)
+    : type_{id}
+    , pdg_{pdg_particle(id)}
+    , charge_{static_cast<int>(pdg->Charge() / 3)} {}
+// particle with a given momentum 3-vector
+inline particle::particle(const pdg_id id, const XYZVector& p3)
+    : particle{id}
+    , mass_{pdg->Mass()}
+    , p_{p3, sqrt(mass_ * mass_ + p_.Mag2())} {}
+// particle in a given direction with a given energy
+inline particle::particle(const pdg_id id, const XYZVector& direction,
+                          const double E)
+    : particle{id}
+    , mass_{pdg->Mass()}
+    , p_{direction.Unit() * sqrt(E * E - mass_ * mass_), E} {}
+// particle with a given 4-momentum (can be off-shell)
+inline particle::particle(const pdg_id id, const XYZTVector& p)
+    : particle{id}, mass_{p.M()}, p_{p} {}
+// RNG constructors usefull in particular for unstable particles with a mass
+// and lifetime that needs to be generated
+inline particle::particle(const pdg_id id, std::shared_ptr<TRandom> rng)
+    : particle(id) {
+  set_mass_lifetime(std::move(rng));
+}
+inline particle::particle(const pdg_id id, const XYZVector& p3,
+                          std::shared_ptr<TRandom> rng)
+    : particle{id} {
+  set_mass_lifetime(std::move(rng));
+  p_.SetXYZM(p.X(), p.Y(), p.Z(), mass_);
+}
+//
+// parent/daughter modifiers
+//
+inline void particle::add_daughter(const int index) {
+  if (daughter_.min > index || daughter_.min < 0) {
+    daughter_.min = index;
+  }
+  if (daughter_.max >= index || daughter_.max < 0) {
+    daughter_.max = index + 1;
+  }
+}
+inline void particle::add_parent(const int index) {
+  if (parent_.min < 0) {
+    parent_.min = index;
+  } else if (parent_.max < 0) {
+    parent_.max = index;
+  } else {
+    tassert(false, "A particle can have only have up to 2 parents"
+                   "(tried to add additional parent to particle '" +
+                       std::string(pdg->Name()) + "')");
+  }
+}
+//
+// momentum transformations
+//
+// rotate from a coordate system where v moved along the z-axis, to the
+// coordate system of v
+// (algorithm taken from TVector3::RotateUz)
+inline void particle::rotate_uz(const particle::XYZVector& v) {
+  const auto uv = v.Unit();
+  const double u1 = uv.X();
+  const double u2 = uv.Y();
+  const double u3 = uv.Z();
+  double up = u1 * u1 + u2 * u2;
+  if (up) {
+    up = sqrt(up);
+    const double px = p_.X();
+    const double py = p_.Y();
+    const double pz = p_.Z();
+    p_ = {(u1 * u3 * px - u2 * py + u1 * up * pz) / up,
+          (u2 * u3 * px + u1 * py + u2 * up * pz) / up,
+          (u3 * u3 * px - px + u3 * up * pz) / up, p_.T()};
+  } else if (u3 < 0) { // phi = 0, theta = pi
+    p_ = {-p.X(), p.Y(), -p.Z(), p.T()};
+  }
+}
+inline void particle::boost(const particle::BetaVector& bv) {
+  lorentzboost b{bv};
+  boost(b);
+}
+
+//
+// private utility functions
+//
+inline void particle::set_mass_lifetime(std::shared_ptr<TRandom> rng) {
+  if (pdg->Stable()) {
+    mass = pdg->Mass();
+    lifetime = 0;
+    else {
+      mass = rng->BreitWigner(pdg->Mass(), pdg->Width());
+      lifetime = rng->Exp(pdg->Lifetime());
+    }
+  }
+}
 
 } // namespace pcsim
 
