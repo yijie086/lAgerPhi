63bc68ec048f3782c160e202939031e0d29f879e
Sylvester Joosten <sylvester.joosten@gmail.com>
2017-04-12 16:10:21 -0400

    added brodksy t-channel cross section

--- pcsim/physics/vm.hh
+++ pcsim/physics/vm.hh
@@ -28,10 +28,10 @@ namespace physics {
 //      R. Fiore et al., "Exclusive Jpsi electroproduction in a dual model."
 //      PRD80:116001, 2009"
 // =============================================================================
-double R_vm_martynov(const double Q2, const particle& vm, const double R_a,
-                     const double R_n) {
+inline double R_vm_martynov(const double Q2, const particle& vm,
+                            const double a, const double n) {
   const double Mv2 = vm.mass * vm.mass;
-  return pow((R_a * Mv2 + Q2) / (R_a * Mv2), R_n) - 1.;
+  return pow((a * Mv2 + Q2) / (a * Mv2), n) - 1.;
 }
 
 // =============================================================================
@@ -47,15 +47,73 @@ double R_vm_martynov(const double Q2, const particle& vm, const double R_a,
 //      Adams et al., "Diffractive production of ρ0 mesons in muon–proton
 //      interactions 470 GeV", ZPC74 (1997) 237-261.
 //
-// An optimized power of 2.575 was taken from a fit result from
+// An optimized power of n=2.575 was taken from a fit result from
 //
 //      M Tytgat, "Diffractive production of ρ0 and ω vector mesons at HERMES",
 //      DESY-Thesis 2001-018 (2001)
 // =============================================================================
-double dipole_ff_vm(const double Q2, const particle& vm,
-                    const double dipole_n) {
+inline double dipole_ff_vm_hermes(const double Q2, const particle& vm,
+                                  const double n) {
   const double Mv2 = vm.mass * vm.mass;
-  return pow(Mv2 / (Mv2 + Q2), dipole_n);
+  return pow(Mv2 / (Mv2 + Q2), n);
+}
+
+// =============================================================================
+// t-channel photo-production cross section for heavy vector mesons
+// Values are in units of nb/GeV^2.
+//
+// Formulism from
+//      brodsky et. al., Phys.Lett.B498:23-28,2001
+//      (http://arxiv.org/abs/hep-ph/0010343)
+//
+// Both dsigma/dt and dsigma/d(exp_bt) are available. The latter is useful for a
+// more efficient MC implementation
+//
+// Parameter values from Eric's fit to the J/psi world data:
+//  * c2g: 6.499e3 [GeV^-2]
+//  * c3g: 2.894e3 [GeV^-2]
+//  * b: 1.13 [GeV^-2]
+//
+// =============================================================================
+inline double dsigma_dt_vm_brodksy(const double s, const double t,
+                                   const particle& target, const particle& vm,
+                                   const double b, const double c2g,
+                                   const double c3g = 0) {
+  const double Mt = target.mass;
+  const double Mt2 = Mt * Mt;
+  const double Mv = vm.mass;
+  const double Mv2 = Mv * Mv;
+  const double x = (2. * Mt * Mv + Mv2) / (s - Mt2);
+  // form factor
+  const double ff = exp(b * t);
+  // phase space factor
+  const double v = 1. / (16. * TMath::Pi());
+  // 2 gluon term
+  const double A2g = c2g * v * (1 - x) * (1 - x) / Mv2;
+  // 3 gluon term
+  const double A3g = c3g * v / (Mv2 * Mv2);
+  return (A2g + A3g) * ff;
+}
+inline double dsigma_dexp_bt_brodksy(const double s, const double t,
+                                     const particle& target, const particle& vm,
+                                     const double b, const double c2g,
+                                     const double c3g = 0) {
+  const double Mt = target.mass;
+  const double Mt2 = Mt * Mt;
+  const double Mv = vm.mass;
+  const double Mv2 = Mv * Mv;
+  const double x = (2. * Mt * Mv + Mv2) / (s - Mt2);
+  // form factor (absorbed by the jacobian for d(bt) -> d(exp_bt)
+  const double ff = 1.;
+  // phase space factor
+  const double v = 1. / (16. * TMath::Pi());
+  // 2 gluon term
+  const double A2g = c2g * v * (1 - x) * (1 - x) / Mv2;
+  // 3 gluon term
+  const double A3g = c3g * v / (Mv2 * Mv2);
+  // extra jacobian for dt -> d(bt)
+  const double jacobian = 1 / b;
+  return (A2g + A3g) * ff * jacobian;
 }
 
 } // physics
