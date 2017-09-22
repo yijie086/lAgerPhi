070ad32f57b3c1c656d52a652556dbdcdb76ea79
Sylvester Joosten <sylvester.joosten@gmail.com>
2017-09-22 18:09:47 -0400

    CMake infrastructure changes to deal with new compiler

---
+++ cmake/debug.cmake
@@ -0,0 +1,5 @@
+## print all variables
+get_cmake_property(_variableNames VARIABLES)
+foreach (_variableName ${_variableNames})
+    message(STATUS "${_variableName}=${${_variableName}}")
+endforeach()
