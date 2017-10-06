525b1da5941a0137dbef396f729f76dc6f9b97f0
Sylvester Joosten <sylvester.joosten@gmail.com>
2017-10-06 16:09:20 -0400

    additional polish

--- pcsim/root/dataframe/dataframe.cc
+++ pcsim/root/dataframe/dataframe.cc
@@ -2,6 +2,12 @@
 #include "lp_gamma.hh"
 #include "anl.hh"
 
+namespace pcsim {
+namespace root {
+namespace dataframe {
 namespace {
 void dummy() {}
-}
+} // namespace
+} // namespace dataframe
+} // namespace root
+} // namespace pcsim
--- pcsim/root/dataframe/lp_gamma.hh
+++ pcsim/root/dataframe/lp_gamma.hh
@@ -16,26 +16,75 @@ namespace dataframe {
 
 class lp_gamma : public custom_dataframe {
 public:
-  using vector_type = ROOT::Math::XYZTVector;
+  // data structures
+  using particle_type = TParticle;
+  using particles_type = TClonesArray;
+  using particle_crefs_type =
+      custom_dataframe::particle_crefs_type<particle_type>;
+  // mc_particle and rc_particle are stored in the exact same structure
+  using mc_particle_type = particle_type;
+  using mc_particles_type = particles_type;
+  using mc_particle_crefs_type = particle_crefs_type;
+  using rc_particle_type = particle_type;
+  using rc_particles_type = particles_type;
+  using rc_particle_crefs_type = particle_crefs_type;
 
+  // column identifiers
+  constexpr static const char* MC_PARTS = "particles";
+  constexpr static const char* RC_PARTS = "rc_particles";
+
+  // tree name
+  constexpr static const char* TREE_NAME = "lp_gamma_event";
+
+  // constructors
   lp_gamma(std::string_view fname, const double scale = 1.)
-      : custom_dataframe{{"lp_gamma_event", fname}, scale} {}
+      : custom_dataframe{{TREE_NAME, fname}, scale} {}
   lp_gamma(const std::vector<std::string>& fnames, const double scale = 1.)
-      : custom_dataframe{{"lp_gamma_event", fnames}, scale} {}
+      : custom_dataframe{{TREE_NAME, fnames}, scale} {}
 
-  // utility functions to get certain 4-vectors from the event
-  static vector_type vector(const TClonesArray& particles,
-                            const int16_t index) {
+  // utility functions to extract data from the event
+  static ROOT::Math::XYZTVector vector(const particles_type& parts,
+                                       const int16_t index) {
+    return vector(*static_cast<particle_type*>(parts.At(index)));
+  }
+  template <int16_t index>
+  ROOT::Math::XYZTVector vector(const particles_type& parts) {
+    return vector(parts, index);
+  }
+  static ROOT::Math::XYZTVector vector(const particle_type& part) {
     TLorentzVector v;
-    static_cast<TParticle*>(particles.At(index))->Momentum(v);
+    part.Momentum(v);
     return {v.X(), v.Y(), v.Z(), v.T()};
   }
   template <int ChildIndex>
-  static vector_type child_vector(const TClonesArray& particles,
-                                  const int16_t parent_index) {
-    const int16_t index = static_cast<TParticle*>(particles.At(parent_index))
+  static ROOT::Math::XYZTVector child_vector(const particles_type& parts,
+                                             const int16_t parent_index) {
+    const int16_t index = static_cast<particle_type*>(parts.At(parent_index))
                               ->GetDaughter(ChildIndex);
-    return vector(particles, index);
+    return vector(parts, index);
+  }
+  template <int pid> static size_t pid_count(const particles_type& parts) {
+    size_t cnt = 0;
+    const size_t N = parts.GetSize();
+    for (size_t i = 0; i < N; ++i) {
+      const auto& part = *static_cast<TParticle*>(parts.At(i));
+      if (part.GetWeight() == 1. && pid == part.GetPdgCode()) {
+        cnt += 1;
+      }
+    }
+    return cnt;
+  }
+  template <int pid>
+  static particle_crefs_type select_pid(const particles_type& parts) {
+    particle_crefs_type selected;
+    const size_t N = parts.GetSize();
+    for (size_t i = 0; i < N; ++i) {
+      const auto& part = *static_cast<TParticle*>(parts.At(i));
+      if (part.GetWeight() == 1. && pid == part.GetPdgCode()) {
+        selected.push_back(part);
+      }
+    }
+    return selected;
   }
 };
 } // namespace dataframe
