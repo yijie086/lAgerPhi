d5df7e27e38033f792fa77b7add0c31eef92ebea
Sylvester Joosten <sjoosten@anl.gov>
2019-04-11 12:10:49 -0600

    fixed rpath for MacOS

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -51,3 +51,13 @@ include_directories(${ROOT_INCLUDE_DIRS})
 ## boost
 find_package(Boost COMPONENTS program_options filesystem system REQUIRED)
 include_directories(${Boost_INCLUDE_DIRS})
+
+## RPATH on MAcOS
+## Set correct rpath on MacOs
+if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+  set(CMAKE_MACOSX_RPATH 1)
+  set(CMAKE_INSTALL_RPATH
+    ${CMAKE_INSTALL_RPATH} 
+    "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
+message("Setting RPath to ${CMAKE_INSTALL_RPATH}")
+endif()
