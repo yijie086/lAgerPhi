ee2c67748faa705f3b90ff2a740deb68d652737f
Sylvester Joosten <sjoosten@anl.gov>
2022-05-09 22:24:08 +0000

    MacOS support

--- install.sh
+++ install.sh
@@ -74,123 +74,124 @@ echo "Setting up development environment for eicweb/$CONTAINER:$VERSION"
 
 mkdir -p $PREFIX/lib || exit 1
 
-SINGULARITY=
-## check for a singularity install
-## default singularity if new enough
-if [ $(type -P singularity ) ]; then
-  SINGULARITY=$(which singularity)
-  SINGULARITY_VERSION=`$SINGULARITY --version`
-  if [ ${SINGULARITY_VERSION:0:1} = 2 ]; then
-    ## too old, look for something else
-    SINGULARITY=
-  fi
-fi
-if [ -z $SINGULARITY ]; then
-  ## first priority: a known good install (this one is on JLAB)
-  if [ -d "/apps/singularity/3.7.1/bin/" ]; then
-    SINGULARITY="/apps/singularity/3.7.1/bin/singularity"
-  ## whatever is in the path is next
-  elif [ $(type -P singularity ) ]; then
+function install_linux() {
+  SINGULARITY=
+  ## check for a singularity install
+  ## default singularity if new enough
+  if [ $(type -P singularity ) ]; then
     SINGULARITY=$(which singularity)
-  ## cvmfs singularity is last resort (sandbox mode can cause issues)
-  elif [ -f "/cvmfs/oasis.opensciencegrid.org/mis/singularity/bin/singularity" ]; then
-    SINGULARITY="/cvmfs/oasis.opensciencegrid.org/mis/singularity/bin/singularity"
-  ## not good...
+    SINGULARITY_VERSION=`$SINGULARITY --version`
+    if [ ${SINGULARITY_VERSION:0:1} = 2 ]; then
+      ## too old, look for something else
+      SINGULARITY=
+    fi
+  fi
+  if [ -z $SINGULARITY ]; then
+    ## first priority: a known good install (this one is on JLAB)
+    if [ -d "/apps/singularity/3.7.1/bin/" ]; then
+      SINGULARITY="/apps/singularity/3.7.1/bin/singularity"
+    ## whatever is in the path is next
+    elif [ $(type -P singularity ) ]; then
+      SINGULARITY=$(which singularity)
+    ## cvmfs singularity is last resort (sandbox mode can cause issues)
+    elif [ -f "/cvmfs/oasis.opensciencegrid.org/mis/singularity/bin/singularity" ]; then
+      SINGULARITY="/cvmfs/oasis.opensciencegrid.org/mis/singularity/bin/singularity"
+    ## not good...
+    else
+      echo "ERROR: no singularity found, please make sure you have singularity in your \$PATH"
+      exit 1
+    fi
+  fi
+  echo " - Found singularity at $SINGULARITY"
+
+  ## get singularity version
+  ## we only care if is 2.x or not, so we can use singularity --version 
+  ## which returns 2.xxxxx for version 2
+  SINGULARITY_VERSION=`$SINGULARITY --version`
+  SIF=
+  if [ ${SINGULARITY_VERSION:0:1} = 2 ]; then
+    SIF="$PREFIX/lib/${CONTAINER}-${VERSION}.simg"
+
+    echo "WARNING: your singularity version $SINGULARITY_VERSION is ancient, we strongly recommend using version 3.x"
+    echo "We will attempt to use a fall-back SIMG image to be used with this singularity version"
+    if [ -f /gpfs02/eic/athena/${CONTAINER}-${VERSION}.simg ]; then
+      ln -sf /gpfs02/eic/athena/${CONTAINER}-${VERSION}.simg ${SIF}
+    else
+      echo "Attempting last-resort singularity pull for old image"
+      echo "This may take a few minutes..."
+      INSIF=`basename ${SIF}`
+      singularity pull --name "${INSIF}" docker://eicweb/$CONTAINER:$VERSION
+      mv ${INSIF} $SIF
+      chmod +x ${SIF}
+      unset INSIF
+    fi
+  ## we are in sane territory, yay!
   else
-    echo "ERROR: no singularity found, please make sure you have singularity in your \$PATH"
+    ## check if we can just use cvmfs for the image
+    SIF="$PREFIX/lib/${CONTAINER}-${VERSION}.sif"
+    if [ -z "$DISABLE_CVMFS_USAGE" -a -d /cvmfs/singularity.opensciencegrid.org/eicweb/${CONTAINER}:${VERSION} ]; then
+      SIF="$PREFIX/lib/${CONTAINER}-${VERSION}"
+      ## need to cleanup in this case, else it will try to make a subdirectory
+      rm -rf ${SIF}
+      ln -sf /cvmfs/singularity.opensciencegrid.org/eicweb/${CONTAINER}:${VERSION} ${SIF}
+    elif [ -f /cvmfs/eic.opensciencegrid.org/singularity/athena/${CONTAINER}_v${VERSION}.sif ]; then
+      ln -sf /cvmfs/eic.opensciencegrid.org/singularity/athena/${CONTAINER}_v${VERSION}.sif ${SIF}
+    elif [ -f /gpfs02/cvmfst0/eic.opensciencegrid.org/singularity/athena/${CONTAINER}_v${VERSION}.sif ]; then
+      ln -sf /gpfs02/cvmfst0/eic.opensciencegrid.org/singularity/athena/${CONTAINER}_v${VERSION}.sif ${SIF}
+    ## check if we have an internal CI image we will use for testing purposes
+    elif [ -f $PWD/.gitlab-ci/${CONTAINER}-${VERSION}.sif ]; then
+      ln -sf $PWD/.gitlab-ci/${CONTAINER}-${VERSION}.sif ${SIF}
+    ## if not, download the container to the system
+    else
+      ## get the python installer and run the old-style install
+      ## work in temp directory
+      tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+      pushd $tmp_dir
+      wget https://eicweb.phy.anl.gov/monte_carlo/lager/-/raw/master/install.py
+      chmod +x install.py
+      ./install.py -f -c $CONTAINER -v $VERSION .
+      INSIF=lib/`basename ${SIF}`
+      mv $INSIF $SIF
+      chmod +x ${SIF}
+      ## cleanup
+      popd
+      rm -rf $tmp_dir
+      unset INSIF
+    fi
+  fi
+
+  echo $SIF
+  ls $SIF 2>&1 > /dev/null && GOOD_SIF=1 
+  if [ -z "$SIF" -o -z "$GOOD_SIF" ]; then
+    echo "ERROR: no singularity image found"
     exit 1
-  fi
-fi
-echo " - Found singularity at $SINGULARITY"
-
-## get singularity version
-## we only care if is 2.x or not, so we can use singularity --version 
-## which returns 2.xxxxx for version 2
-SINGULARITY_VERSION=`$SINGULARITY --version`
-SIF=
-if [ ${SINGULARITY_VERSION:0:1} = 2 ]; then
-  SIF="$PREFIX/lib/${CONTAINER}-${VERSION}.simg"
-
-  echo "WARNING: your singularity version $SINGULARITY_VERSION is ancient, we strongly recommend using version 3.x"
-  echo "We will attempt to use a fall-back SIMG image to be used with this singularity version"
-  if [ -f /gpfs02/eic/athena/${CONTAINER}-${VERSION}.simg ]; then
-    ln -sf /gpfs02/eic/athena/${CONTAINER}-${VERSION}.simg ${SIF}
   else
-    echo "Attempting last-resort singularity pull for old image"
-    echo "This may take a few minutes..."
-    INSIF=`basename ${SIF}`
-    singularity pull --name "${INSIF}" docker://eicweb/$CONTAINER:$VERSION
-    mv ${INSIF} $SIF
-    chmod +x ${SIF}
-    unset INSIF
+    echo " - Deployed ${CONTAINER} image: $SIF"
   fi
-## we are in sane territory, yay!
-else
-  ## check if we can just use cvmfs for the image
-  SIF="$PREFIX/lib/${CONTAINER}-${VERSION}.sif"
-  if [ -z "$DISABLE_CVMFS_USAGE" -a -d /cvmfs/singularity.opensciencegrid.org/eicweb/${CONTAINER}:${VERSION} ]; then
-    SIF="$PREFIX/lib/${CONTAINER}-${VERSION}"
-    ## need to cleanup in this case, else it will try to make a subdirectory
-    rm -rf ${SIF}
-    ln -sf /cvmfs/singularity.opensciencegrid.org/eicweb/${CONTAINER}:${VERSION} ${SIF}
-  elif [ -f /cvmfs/eic.opensciencegrid.org/singularity/athena/${CONTAINER}_v${VERSION}.sif ]; then
-    ln -sf /cvmfs/eic.opensciencegrid.org/singularity/athena/${CONTAINER}_v${VERSION}.sif ${SIF}
-  elif [ -f /gpfs02/cvmfst0/eic.opensciencegrid.org/singularity/athena/${CONTAINER}_v${VERSION}.sif ]; then
-    ln -sf /gpfs02/cvmfst0/eic.opensciencegrid.org/singularity/athena/${CONTAINER}_v${VERSION}.sif ${SIF}
-  ## check if we have an internal CI image we will use for testing purposes
-  elif [ -f $PWD/.gitlab-ci/${CONTAINER}-${VERSION}.sif ]; then
-    ln -sf $PWD/.gitlab-ci/${CONTAINER}-${VERSION}.sif ${SIF}
-  ## if not, download the container to the system
-  else
-    ## get the python installer and run the old-style install
-    ## work in temp directory
-    tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
-    pushd $tmp_dir
-    wget https://eicweb.phy.anl.gov/monte_carlo/lager/-/raw/master/install.py
-    chmod +x install.py
-    ./install.py -f -c $CONTAINER -v $VERSION .
-    INSIF=lib/`basename ${SIF}`
-    mv $INSIF $SIF
-    chmod +x ${SIF}
-    ## cleanup
-    popd
-    rm -rf $tmp_dir
-    unset INSIF
-  fi
-fi
 
-echo $SIF
-ls $SIF 2>&1 > /dev/null && GOOD_SIF=1 
-if [ -z "$SIF" -o -z "$GOOD_SIF" ]; then
-  echo "ERROR: no singularity image found"
-  exit 1
-else
-  echo " - Deployed ${CONTAINER} image: $SIF"
-fi
-
-## We want to make sure the root directory of the install directory
-## is always bound. We also check for the existence of a few standard
-## locations (/scratch /volatile /cache) and bind those too if found
-echo " - Determining additional bind paths"
-PREFIX_ROOT="/$(realpath $PREFIX | cut -d "/" -f2)"
-BINDPATH=$PREFIX_ROOT
-echo "   --> $PREFIX_ROOT"
-for dir in /work /scratch /volatile /cache /gpfs /gpfs01 /gpfs02; do
-  ## only add directories once
-  if [[ ${BINDPATH} =~ `basename $dir` ]]; then
-    continue
-  fi
-  if [ -d $dir ]; then
-    echo "   --> $dir"
-    BINDPATH="${BINDPATH},$dir"
-  fi
-done
+  ## We want to make sure the root directory of the install directory
+  ## is always bound. We also check for the existence of a few standard
+  ## locations (/scratch /volatile /cache) and bind those too if found
+  echo " - Determining additional bind paths"
+  PREFIX_ROOT="/$(realpath $PREFIX | cut -d "/" -f2)"
+  BINDPATH=$PREFIX_ROOT
+  echo "   --> $PREFIX_ROOT"
+  for dir in /work /scratch /volatile /cache /gpfs /gpfs01 /gpfs02; do
+    ## only add directories once
+    if [[ ${BINDPATH} =~ `basename $dir` ]]; then
+      continue
+    fi
+    if [ -d $dir ]; then
+      echo "   --> $dir"
+      BINDPATH="${BINDPATH},$dir"
+    fi
+  done
 
 
-mkdir -p $PREFIX/bin
+  mkdir -p $PREFIX/bin
 
-## create a new top-level lager-shell launcher script
-## that starts a shell within the lager container
+  ## create a new top-level lager-shell launcher script
+  ## that starts a shell within the lager container
 cat << EOF > $PREFIX/bin/lager-shell
 #!/bin/bash
 
@@ -277,33 +278,173 @@ export SINGULARITY_BINDPATH=$BINDPATH
 $SINGULARITY exec $SIF lager-shell \$@
 EOF
 
-## raw launcher for lager
+  ## raw launcher for lager
 cat << EOF > $PREFIX/bin/lager
 #!/bin/bash
 export SINGULARITY_BINDPATH=$BINDPATH
 $SINGULARITY exec $SIF lager \$@
 EOF
 
-chmod +x $PREFIX/bin/lager-shell
-chmod +x $PREFIX/bin/lager
+  chmod +x $PREFIX/bin/lager-shell
+  chmod +x $PREFIX/bin/lager
 
-## also get some example configs
-if [ -d $PREFIX/share/lager-examples ]; then
-  rm -rf $PREFIX/share/lager-examples
+  ## also get some example configs
+  if [ -d $PREFIX/share/lager-examples ]; then
+    rm -rf $PREFIX/share/lager-examples
+  fi
+
+  git clone --depth=1 https://eicweb.phy.anl.gov/monte_carlo/lager.git lager-tmp \
+    && mkdir -p $PREFIX/share \
+    && mv lager-tmp/examples share/lager-examples
+  ## always cleanup
+  rm -rf lager-tmp
+
+  echo " - Created custom lager excecutable"
+  echo " - Created custom lager-shell excecutable"
+  echo " - Downloaded examples into $PREFIX/share/lager-examples"
+}
+
+function install_macos() {
+  ## check for docker install
+  DOCKER=$(which docker)
+  if [ -z ${DOCKER} ]; then
+    echo "ERROR: no docker install found, docker is required for running on MacOS"
+  fi
+  echo " - Found docker at ${DOCKER}"
+
+  IMG=eicweb/${CONTAINER}:${VERSION}
+  docker pull ${IMG}
+  echo " - Deployed ${CONTAINER} image: ${IMG}"
+
+  ## We want to make sure the root directory of the install directory
+  ## is always bound. We also check for the existence of a few standard
+  ## locations (/Volumes /Users /tmp) and bind those too if found
+  echo " - Determining mount paths"
+  PREFIX_ROOT="/$(realpath $PREFIX | cut -d "/" -f2)"
+  MOUNT=""
+  echo "   --> $PREFIX_ROOT"
+  for dir in /Volumes /Users /tmp; do
+    ## only add directories once
+    if [[ ${MOUNT} =~ $(basename $dir) ]]; then
+      continue
+    fi
+    if [ -d $dir ]; then
+      echo "   --> $dir"
+      MOUNT="$MOUNT -v $dir:$dir"
+    fi
+  done
+  echo " - Docker mount directive: '$MOUNT'"
+
+  ## create a new top-level lager-shell launcher script
+  ## that sets the ATHENA_PREFIX and then starts singularity
+cat << EOF > $PREFIX/bin/lager-shell
+#!/bin/bash
+
+## capture environment setup for upgrades
+CONTAINER=$CONTAINER
+TMPDIR=$TMPDIR
+VERSION=$VERSION
+PREFIX=$PREFIX
+DISABLE_CVMFS_USAGE=${DISABLE_CVMFS_USAGE}
+
+function print_the_help {
+  echo "USAGE:  ./lager-shell [OPTIONS] [ -- COMMAND ]"
+  echo "OPTIONAL ARGUMENTS:"
+  echo "          -u,--upgrade    Upgrade the container to the latest version"
+  echo "          -h,--help       Print this message"
+  echo ""
+  echo "  Start the lager-shell containerized software environment."
+  echo ""
+  echo "EXAMPLES: "
+  echo "  - Start an interactive shell: ./lager-shell" 
+  echo "  - Upgrade the container:      ./lager-shell --upgrade"
+  echo "  - Execute a single command:   ./lager-shell -- <COMMAND>"
+  echo ""
+  exit
+}
+
+UPGRADE=
+
+while [ \$# -gt 0 ]; do
+  key=\$1
+  case \$key in
+    -u|--upgrade)
+      UPGRADE=1
+      shift
+      ;;
+    -h|--help)
+      print_the_help
+      exit 0
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "ERROR: unknown argument: \$key"
+      echo "use --help for more info"
+      exit 1
+      ;;
+  esac
+done
+
+if [ ! -z \${UPGRADE} ]; then
+  echo "Upgrading lager-shell..."
+  docker pull $IMG || exit 1
+  echo "lager-shell upgrade sucessful"
+  exit 0
 fi
 
-git clone --depth=1 https://eicweb.phy.anl.gov/monte_carlo/lager.git lager-tmp \
-  && mkdir -p $PREFIX/share \
-  && mv lager-tmp/examples share/lager-examples
-## always cleanup
-rm -rf lager-tmp
+docker run $MOUNT -w=$PWD -it --rm $IMG lager-shell \$@
+EOF
 
-echo " - Created custom lager excecutable"
-echo " - Created custom lager-shell excecutable"
-echo " - Downloaded examples into $PREFIX/share/lager-examples"
+  ## raw launcher for lager
+cat << EOF > $PREFIX/bin/lager
+#!/bin/bash
+dokcer run $MOUNT -w=$PWD -it --rm $IMG lager \$@
+EOF
+
+  chmod +x $PREFIX/bin/lager
+  chmod +x $PREFIX/bin/lager-shell
+
+  ## also get some example configs
+  if [ -d $PREFIX/share/lager-examples ]; then
+    rm -rf $PREFIX/share/lager-examples
+  fi
+
+  git clone --depth=1 https://eicweb.phy.anl.gov/monte_carlo/lager.git lager-tmp \
+    && mkdir -p $PREFIX/share \
+    && mv lager-tmp/examples share/lager-examples
+  ## always cleanup
+  rm -rf lager-tmp
+
+  echo " - Created custom lager excecutable"
+  echo " - Created custom lager-shell excecutable"
+  echo " - Downloaded examples into $PREFIX/share/lager-examples"
+}
+
+## detect OS
+OS=`uname -s`
+case ${OS} in
+  Linux)
+    echo " - Detected OS: Linux"
+    install_linux
+    ;;
+  Darwin)
+    echo " - Detected OS: MacOS"
+    install_macos
+    ;;
+  *)
+    echo "ERROR: OS '${OS}' not currently supported"
+    exit 1
+    ;;
+esac
+
+popd
+
+echo "Environment setup succesfull"
+echo "You can start the development environment by running './eic-shell'"
 
 echo "Environment setup succesfull"
 echo "You can start the lager by running $PREFIX/bin/lager (see lager --help for more info)."
 echo "You can start a full development environment by running '$PREFIX/bin/lager-shell'"
-
-popd
